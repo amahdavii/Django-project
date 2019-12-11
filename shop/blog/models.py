bfdf82f91728f908910f94af37e803deb6b957e3
Ali Mahdavi Amiri <amahdavi9978@gmail.com>
2019-12-11 20:36:05 +0330

    add psycopg2 package in venv

--- shop/blog/models.py
+++ shop/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+
+    title = models.CharField(max_length = 60)
+    slug = models.SlugField(max_length = 100)
+    body = models.TextField()
+    published = models.DataTime(default = timezone.now)
+    created = models.DataTime(auto_now_add = True)
+    updated = models.DataTime(auto_now = True)
+    status = models.CharField(max_length = 60, choices = STATUS_CHOICES, default = 'draft')
+    
--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -77,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'learnfiles',
-        'PASSWORD': '1234',
+        'PASSWORD': '12345',
         'USER': 'postgres',
         'PORT': 5432
     }
