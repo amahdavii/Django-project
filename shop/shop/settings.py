771e8203e98cd327e7bcf470fff28ecf48ef8072
Ali Mahdavi Amiri <amahdavi9978@gmail.com>
2019-12-11 17:49:43 +0330

    postgresql setup is done.

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'learnfiles',
+        'PASSWORD': '1234',
+        'USER': 'postgres',
+        'PORT': 5432
     }
 }
 
