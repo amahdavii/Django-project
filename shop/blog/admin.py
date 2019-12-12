b77e8963c1bfbd231567880efef985599c5dba71
Ali Mahdavi Amiri <amahdavi9978@gmail.com>
2019-12-12 15:30:25 +0330

    add new features to blog admin panel.

--- shop/blog/admin.py
+++ shop/blog/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Post
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','slug' , 'published', 'status')
+    list_filter = ('published', 'status')
+    search_fields = ('title', 'body')
+    ordering = ['status', 'published']
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post, PostAdmin)
