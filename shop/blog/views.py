19459b51bd7f462365f7c7dcede463d15b898d9c
Ali Mahdavi Amiri <amahdavi9978@gmail.com>
2019-12-13 11:54:43 +0330

    Optimize code and adhere to dry principles.

--- shop/blog/views.py
+++ shop/blog/views.py
@@ -1,21 +1,17 @@
-from django.template import loader
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import Http404
 from .models import Post
 # Create your views here.
 def index(request):
     latest_posts_list = Post.objects.order_by('-published')[:5]
-    #output = ", \n".join([p.slug for p in latest_posts_list])
-    template = loader.get_template('index.html')
     context = {
         'latest_posts_list': latest_posts_list,
     }
-    return HttpResponse(template.render(context, request))
-    
+    return render(request, 'index.html', context)
+
 def detail(request, post_id):
     post = get_object_or_404(Post, pk = post_id)
-    template = loader.get_template('detail.html')
     context = {
         'post': post,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'detail.html', context)
